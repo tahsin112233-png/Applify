3c96e7276a5753fadad8057701f1d5612ac01eba
tahsin112233-png <akash.afnan112233@gmail.com>
2026-04-28 23:24:31 +0600

    Update MiniPlayer.tsx

--- src/components/MiniPlayer.tsx
+++ src/components/MiniPlayer.tsx
@@ -3,26 +3,32 @@ import { config } from "@utils";
 import { LikeButton, MediaDetails, PlayButton, PlayNextButton } from "./MediaPartials";
 import { playerStore, setNavStore, queueStore } from "@stores";
 
+const MediaArtwork = lazy(() => import('@components/MediaPartials/MediaArtwork'));
 
-const MediaArtwork = lazy(() => import('@components/MediaPartials/MediaArtwork'))
-
-export default function() {
-
+export default function () {
   return (
-    <div class='miniplayer' onclick={(e) => {
-      if (!e.target.matches('button'))
-        setNavStore('player', 'state', true);
-    }
-    }>
-      <progress value={((playerStore.currentTime / playerStore.fullDuration) || 0).toFixed(3)}></progress>
+    <div
+      class='miniplayer'
+      onclick={(e) => {
+        if (!(e.target as HTMLElement).closest('button'))
+          setNavStore('player', 'state', true);
+      }}
+    >
+      <progress
+        value={((playerStore.currentTime / playerStore.fullDuration) || 0).toFixed(3)}
+      />
+
       <Show when={config.loadImage}>
         <MediaArtwork />
       </Show>
+
       <MediaDetails />
+
       <PlayButton />
+
       <Show when={queueStore.list.length} fallback={<LikeButton />}>
         <PlayNextButton />
       </Show>
     </div>
-  )
+  );
 }
