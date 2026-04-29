e8438788872e179b3da10fa7e10b31f77fdf7df1
tahsin112233-png <akash.afnan112233@gmail.com>
2026-04-29 08:02:29 +0600

    Update app.ts

--- src/lib/stores/app.ts
+++ src/lib/stores/app.ts
@@ -14,7 +14,7 @@ const storeInit: {
   locale: string,
   translations: Record<TranslationKeys, string> | {}
 } = {
-  api: import.meta.env.DEV ? '' : 'https://api.ytify.workers.dev',
+  api: '',
   useSaavn: true,
   locale: initLocale,
   translations: {},
@@ -24,19 +24,14 @@ export const [store, setStore] = createStore(storeInit);
 
 
 export function t(key: TranslationKeys, value: string = ''): string {
-
   const translations = store.translations as Record<TranslationKeys, string>;
   const translatedString = translations[key] || key as string;
   return value ? translatedString.replace('$', value) : translatedString;
 }
 
 export async function updateLang() {
-
   document.documentElement.lang = store.locale;
-
-  const json = await import(`../../locales/${store.locale}.json`)
-
+  const json = await import(`../../locales/${store.locale}.json`);
   setStore('translations', json.default);
   return true;
-
 }
